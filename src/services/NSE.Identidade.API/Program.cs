1b834dc3da61ac696cd20c075c6b60da52c71668
gustavohrgomes <dev.gustavogomes@gmail.com>
2022-06-21 18:41:27 -0300

    Registro e Login de usuário

--- src/services/NSE.Identidade.API/Program.cs
+++ src/services/NSE.Identidade.API/Program.cs
@@ -20,7 +20,7 @@ var app = builder.Build();
         app.UseSwaggerUI();
     }
 
-    app.UseHttpsRedirection();
+    //app.UseHttpsRedirection();
 
     app.UseAuthentication();
     app.UseAuthorization();
