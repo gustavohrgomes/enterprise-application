5eeb8a908477baa917b8752bf35cdbf083d509b3
gustavohrgomes <dev.gustavogomes@gmail.com>
2022-06-22 07:30:25 -0300

    Emitindo JWT pela Controller

--- src/services/NSE.Identidade.API/Controllers/AuthController.cs
+++ src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using NSE.Identidade.API.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace NSE.Identidade.API.Controllers;
 
@@ -10,12 +14,15 @@ public class AuthController : Controller
 {
     private readonly SignInManager<IdentityUser> _signInManager;
     private readonly UserManager<IdentityUser> _userManager;
+    private readonly IConfiguration _configuration;
 
-    public AuthController(SignInManager<IdentityUser> signInManager, 
-                          UserManager<IdentityUser> userManager)
+    public AuthController(SignInManager<IdentityUser> signInManager,
+                          UserManager<IdentityUser> userManager, 
+                          IConfiguration configuration)
     {
         _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
         _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration)); ;
     }
 
     [HttpPost("nova-conta")]
@@ -35,7 +42,7 @@ public class AuthController : Controller
         if (result.Succeeded)
         {
             await _signInManager.SignInAsync(user, false);
-            return Ok();
+            return Ok(await GerarJwtToken(usuarioRegistro.Email));
         }
 
         return BadRequest();
@@ -52,9 +59,59 @@ public class AuthController : Controller
 
         var result = await _signInManager.PasswordSignInAsync(user, usuarioLogin.Senha, false, true);
 
-        if (result.Succeeded) return Ok();
+        if (result.Succeeded) return Ok(await GerarJwtToken(usuarioLogin.Email));
 
         return BadRequest();
     }
+
+    private async Task<UsuarioRespostaLogin> GerarJwtToken(string email)
+    {
+        var user = await _userManager.FindByEmailAsync(email);
+        var claims = await _userManager.GetClaimsAsync(user);
+        var roles = await _userManager.GetRolesAsync(user);
+
+        claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+
+        foreach (var role in roles)
+            claims.Add(new Claim("role", role));
+
+        var identityClaims = new ClaimsIdentity();
+        identityClaims.AddClaims(claims);
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_configuration.GetValue<string>("AppSettings:Secret"));
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Issuer = _configuration.GetValue<string>("AppSettings:Emissor"),
+            Audience = _configuration.GetValue<string>("AppSettings:ValidoEm"),
+            Subject = identityClaims,
+            Expires = DateTime.UtcNow.AddHours(_configuration.GetValue<int>("AppSettings:ExpiracaoHoras")),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        var encodedToken = tokenHandler.WriteToken(token);
+
+        return new UsuarioRespostaLogin
+        {
+            AccessToken = encodedToken,
+            ExpiresIn = TimeSpan.FromHours(_configuration.GetValue<int>("AppSettings:ExpiracaoHoras")).TotalSeconds,
+            UsuarioToken = new UsuarioToken
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Claims = claims.Select(c => new UsuarioClaim { Type = c.Type, Value = c.Value })
+            }
+        };
+    }
+
+    private static long ToUnixEpochDate(DateTime date)
+        => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
 }
 
