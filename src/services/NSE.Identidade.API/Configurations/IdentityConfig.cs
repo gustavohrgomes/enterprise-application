496b9d7bda4917e90aab501010bcf8fde0f424cd
gustavohrgomes <dev.gustavogomes@gmail.com>
2022-06-23 07:15:00 -0300

    Customizando mensagens do Identity

--- src/services/NSE.Identidade.API/Configurations/IdentityConfig.cs
+++ src/services/NSE.Identidade.API/Configurations/IdentityConfig.cs
@@ -3,44 +3,46 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using NSE.Identidade.API.Data;
+using NSE.Identidade.API.Extensions;
 using System.Text;
 
-namespace NSE.Identidade.API.Configurations
+namespace NSE.Identidade.API.Configurations;
+
+public static class IdentityConfig
 {
-    public static class IdentityConfig
+    public static IServiceCollection AddIdentityconfiguration(this IServiceCollection services, IConfiguration configuration)
     {
-        public static IServiceCollection AddIdentityconfiguration(this IServiceCollection services, IConfiguration configuration)
+        services.AddDbContext<ApplicationDbContext>(options
+            => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddDefaultIdentity<IdentityUser>()
+            .AddRoles<IdentityRole>()
+            .AddErrorDescriber<IdentityMensagensPortugues>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        // JWT
+
+        services.AddAuthentication(opt =>
         {
-            services.AddDbContext<ApplicationDbContext>(options
-                => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-
-            // JWT
-
-            services.AddAuthentication(opt =>
+            opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(bearerOptions =>
+        {
+            bearerOptions.RequireHttpsMetadata = false;
+            bearerOptions.SaveToken = true;
+            bearerOptions.TokenValidationParameters = new TokenValidationParameters
             {
-                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(bearerOptions =>
-            { 
-                bearerOptions.RequireHttpsMetadata = false;
-                bearerOptions.SaveToken = true;
-                bearerOptions.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration.GetValue<string>("AppSettings:Secret"))),
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidAudience = configuration.GetValue<string>("AppSettings:ValidoEm"),
-                    ValidIssuer = configuration.GetValue<string>("AppSettings:Emissor")
-                };
-            });
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration.GetValue<string>("AppSettings:Secret"))),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = configuration.GetValue<string>("AppSettings:ValidoEm"),
+                ValidIssuer = configuration.GetValue<string>("AppSettings:Emissor")
+            };
+        });
 
-            return services;
-        }
+        return services;
     }
 }
+
