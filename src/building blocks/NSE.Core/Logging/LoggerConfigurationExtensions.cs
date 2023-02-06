f7ccbc5d9aa69358b26d28bb8149f6b144cafc84
gustavohrgomes <dev.gustavogomes@gmail.com>
2023-02-06 08:27:20 -0300

    Atualiza a aplicação para enviar informações de logs para o elasticsearch/kibana

--- src/building blocks/NSE.Core/Logging/LoggerConfigurationExtensions.cs
+++ src/building blocks/NSE.Core/Logging/LoggerConfigurationExtensions.cs
@@ -1,19 +1,26 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace NSE.Core.Logging;
 
 public static class LoggerConfigurationExtensions
 {
-    public static void Configure(this LoggerConfiguration loggerConfiguration, IConfiguration configuration)
+    public static void Configure(this LoggerConfiguration loggerConfiguration, HostBuilderContext context)
     {
         ArgumentNullException.ThrowIfNull(loggerConfiguration, nameof(loggerConfiguration));
 
-        ArgumentNullException.ThrowIfNull(configuration, nameof(configuration));
+        ArgumentNullException.ThrowIfNull(context, nameof(context));
 
         loggerConfiguration
             .Enrich.WithProperty("Host", Environment.MachineName)
             .Enrich.WithProperty("OS", Environment.OSVersion)
-            .ReadFrom.Configuration(configuration);
+            .Enrich.WithProperty("Service", context.Configuration["ApplicationName"])
+            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(context.Configuration["Elasticsearch:Url"]))
+            {
+                IndexFormat = $"{context.Configuration["ApplicationName"]}-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+            }) 
+            .ReadFrom.Configuration(context.Configuration);
     }
 }
