e89fd247347c49aceabdcef61649a72eb973d91b
gustavohrgomes <dev.gustavogomes@gmail.com>
2023-09-03 12:30:08 -0300

    :construction: wip: adicionando mass transit na api de catalogo
    cria PedidoRealizadoConsumer com no CatalogoIntegrationHandler
    adiciona masstransit na injeção de dependência da api de catalogo
    remove referências do message bus de alguns projetos
    remove CatalogoIntegrationHandler

---
+++ src/services/NSE.Carrinho.API/Services/PedidoRealizadoConsumer.cs
@@ -0,0 +1,30 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using NSE.Carrinho.API.Data;
+using NSE.Core.Messages.IntegrationEvents;
+
+namespace NSE.Carrinho.API.Services;
+
+public class PedidoRealizadoConsumer : IConsumer<PedidoRealizadoIntegrationEvent>
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public PedidoRealizadoConsumer(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task Consume(ConsumeContext<PedidoRealizadoIntegrationEvent> context)
+    {
+        using var scope = _serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<CarrinhoContext>();
+
+        var carrinho = await dbContext.CarrinhoCliente.FirstOrDefaultAsync(c => c.ClienteId == context.Message.ClienteId);
+
+        if (carrinho is not null)
+        {
+            dbContext.CarrinhoCliente.Remove(carrinho);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
