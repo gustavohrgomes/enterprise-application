bce6b16eb7c53dbff242e7f941a1da67cc111ab5
gustavohrgomes <dev.gustavogomes@gmail.com>
2023-10-01 15:24:57 -0300

    feat: create resilient transaction in unit of work

--- src/building blocks/NSE.Core/Data/IUnitOfWork.cs
+++ src/building blocks/NSE.Core/Data/IUnitOfWork.cs
@@ -1,9 +1,11 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace NSE.Core.Data;
 
 public interface IUnitOfWork
 {
     Task<bool> CommitAsync();
+    Task<bool> ResilientCommitAsync(CancellationToken cancellationToken);
     IEnumerable<INotification> ExtractDomainEventsFromAggregates();
 }
--- src/building blocks/NSE.Core/Data/UnitOfWork.cs
+++ src/building blocks/NSE.Core/Data/UnitOfWork.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using NSE.Core.Communication;
 using NSE.Core.DomainObjects;
 
 namespace NSE.Core.Data;
@@ -8,16 +10,76 @@ public class UnitOfWork<TDbContext> : IUnitOfWork
     where TDbContext : DbContext
 {
     private readonly TDbContext _context;
+    private readonly IPublisher _publisher;
 
-    public UnitOfWork(TDbContext context)
+    public UnitOfWork(TDbContext context, IPublisher publisher)
     {
         _context = context;
+        _publisher = publisher;
     }
 
     public async Task<bool> CommitAsync()
     {
         UpdateAuditableEntities();
-        return await _context.SaveChangesAsync() > 0;
+        return await _context.SaveChangesAsync().ConfigureAwait(continueOnCapturedContext: false) > 0;
+    }
+
+    public async Task<bool> ResilientCommitAsync(CancellationToken cancellationToken = default)
+    {
+        var strategy = _context.Database.CreateExecutionStrategy();
+
+        var result = await strategy.ExecuteAsync(async () => await TryExecuteStrategy(cancellationToken)
+                .ConfigureAwait(continueOnCapturedContext: false))
+            .ConfigureAwait(continueOnCapturedContext: false);
+
+        return result;
+    }
+
+    private async Task<bool> TryExecuteStrategy(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await _context.Database.BeginTransactionAsync(cancellationToken)
+                .ConfigureAwait(continueOnCapturedContext: false);
+
+            var domainEvents = ExtractDomainEventsFromAggregates();
+
+            var result = await CommitAsync()
+                .ConfigureAwait(continueOnCapturedContext: false);
+
+            await _context.Database.CommitTransactionAsync(cancellationToken)
+                .ConfigureAwait(continueOnCapturedContext: false);
+
+            var dispatchingTasks = domainEvents.Select(x => _publisher.Publish(x, cancellationToken));
+
+            await Task.WhenAll(dispatchingTasks);
+
+            return result;
+        }
+        catch
+        {
+            await _context.Database.RollbackTransactionAsync(cancellationToken)
+                .ConfigureAwait(continueOnCapturedContext: false);
+
+            throw;
+        }
+    }
+    
+    public IEnumerable<INotification> ExtractDomainEventsFromAggregates()
+    {
+        var aggregates = _context.ChangeTracker
+            .Entries<IAggregateRoot>()
+            .Where(x => x.Entity.DomainEvents.Any())
+            .ToList();
+        
+        var domainEvents = aggregates.SelectMany(x => x.Entity.DomainEvents).ToList();
+        
+        foreach (var aggregateEntry in aggregates)
+        {
+            aggregateEntry.Entity.ClearDomainEvents();
+        }
+
+        return domainEvents;
     }
     
     private void UpdateAuditableEntities()
@@ -35,23 +97,4 @@ public class UnitOfWork<TDbContext> : IUnitOfWork
             }
         }
     }
-
-    public IEnumerable<INotification> ExtractDomainEventsFromAggregates()
-    {
-        var aggregates = _context.ChangeTracker
-            .Entries<IAggregateRoot>()
-            .Where(x => x.Entity.DomainEvents.Any())
-            .ToList();
-        
-        var domainEvents = aggregates.SelectMany(x => x.Entity.DomainEvents).ToList();
-        
-        foreach (var aggregateEntry in aggregates)
-        {
-            aggregateEntry.Entity.ClearDomainEvents();
-        }
-
-        return domainEvents;
-    }
-
-   
 }
